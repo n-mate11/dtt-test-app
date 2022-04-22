3db2517df8fe47ad874ba0d8da1762002e3dfed9
Máté Nagy <nagymatepeter@student.vu.nl>
2022-04-22 19:06:48 +0200

    splash screen done, bottom navigation added with two fragments

---
+++ app/src/main/java/com/example/dtttestapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.dtttestapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    HomeFragment homeFragment = new HomeFragment();
+    InfoFragment infoFragment = new InfoFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+                return true;
+            case R.id.info:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, infoFragment).commit();
+                return true;
+
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/example/dtttestapplication/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.dtttestapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME);
+    }
+}
